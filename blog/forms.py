791a0a8f37c2eb024ae5b378149abc934863e944
mary <marianagaleano846@gmail.com>
2024-07-27 11:08:01 -0400

    Primer commit: Inicialización del proyecto Django

--- blog/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ArticleForm(forms.Form):
-    title = forms.CharField(label='Title', max_length=255)
-    content = forms.CharField(label='Content', widget=forms.Textarea())
